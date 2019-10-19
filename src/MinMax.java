e7af8592a60a86d1f83c8218449616cc50b98fd2
dexterchan <dexterchan@gmail.com>
2019-10-19 20:30:15 +0800

    minmax

--- src/MinMax.java
+++ src/MinMax.java
@@ -33,17 +33,18 @@ public class MinMax {
     public static void main(String[] args) {
         int[] arr = new int[5];
 
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        //String[] arrItems = scanner.nextLine().split(" ");
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < 5; i++) {
+        String []arrItems = new String[]{"1","2","5","6"};
+        for (int i = 0; i < arrItems.length; i++) {
             int arrItem = Integer.parseInt(arrItems[i]);
             arr[i] = arrItem;
         }
 
         miniMaxSum(arr);
 
-        scanner.close();
+        //scanner.close();
     }
 }
 
